fc14c5074c9bcd4b20e58ba5275954800a22fff0
Andy <a4heng@ucsd.edu>
2020-11-17 23:45:14 -0800

    Created basic sidebar component

---
+++ src/components/SideNavTabs.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import * as AiIcons from 'react-icons/ai';
+import * as GiIcons from 'react-icons/gi';
+import * as Vscicons from 'react-icons/vsc';
+//GiAcorn
+
+export const SideNavTabs = [
+  {
+    title: 'Home',
+    path:'/',
+    icon: <Vscicons.VscSquirrel />,
+    cName:'nav-text'
+  },
+  {
+    title: 'DataGen',
+    path:'/dataGen',
+    icon: <GiIcons.GiAcorn/>,
+    cName:'nav-text'
+  },
+  //AiOutlinePieChart
+  {
+    title: 'DataViz',
+    path:'/dataViz',
+    icon: <AiIcons.AiOutlinePieChart/>,
+    cName:'nav-text'
+  },
+  {
+    title: 'About',
+    path:'/About',
+    icon: <GiIcons.GiLeafSwirl/>,
+    cName:'nav-text'
+  },
+]

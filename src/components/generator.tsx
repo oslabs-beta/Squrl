ab0c917f4ebeb1d4566dd2034b476cbdd6a41826
Andy <a4heng@ucsd.edu>
2020-11-18 20:45:16 -0800

    able to generate inputs when button is clicked

--- src/components/generator.tsx
+++ src/components/generator.tsx
@@ -1,15 +1,107 @@
-import React from 'react';
-import Fields from './generatorChildren/fields'
-import Exports from './generatorChildren/exports' 
+// import React, { useEffect, useState } from 'react';
+// import Fields from './generatorChildren/fields'
+// import Exports from './generatorChildren/exports'
 
-const Generator = () => {
-    return (
-        <div>
-          <h1>Dummy Data Generator</h1>
-          <Fields />
-          <Exports />
-        </div>
-    )
+// type inputData = {
+//   value: string;
+//   dataType: string;
+//   empty: number;
+// }
+// const initialInput = {
+//   value : '',
+//   dataType : '',
+//   empty: 0
+// }
+// const Generator: React.FC = () => {
+//   const [numOfField, setNumOfField] = useState<number>(0);
+//   const [field, setField] = useState<JSX.Element[]>([]);
+//   const [input, setInput] = useState<inputData[]>([initialInput])
+
+//   const createField = () => {
+//     setField(field => [...field, 
+//       <Fields 
+//         key={numOfField} 
+//         number={numOfField}
+//         value={initialInput.value}
+//         dataType={initialInput.dataType}
+//         empty={initialInput.empty}
+//         callback = {handleChange}
+//       />])
+//   }
+//   const handleChange = (e: any) => {
+//     const {className , value, name} = e.target
+//     let temp = [...input];
+//     console.log("temp: ", temp)
+//     temp[parseInt(className)].value += value;
+//     console.log("input ", temp)
+//     setInput(temp)
+//   }
+    
+//     const increaseNum = () => {
+//       setNumOfField(numOfField + 1);
+//       createField();
+//       console.log(input)
+//   }
+
+//   return (
+//     <div id="gencontainer">
+//       <h1>Dummy Data Generator</h1>
+//       {field}
+//       <button onClick={increaseNum} id="addColumn">Add Another Column</button>
+//       <Exports />
+//     </div>
+//   )
+// }
+// export default Generator
+
+import React, { useState } from 'react';
+import Fields from './generatorChildren/fields'
+import Exports from './generatorChildren/exports'
+
+
+const Generator: React.FC = () => {
+
+  // return (
+  //   <div id="gencontainer">
+  //     <h1>Dummy Data Generator</h1>
+
+  //     <button id="addColumn">Add Another Column</button>
+  //     <Exports />
+  //   </div>
+  // )
+
+  const [values, setValues] = useState<string[]>([])
+
+  function addInput() {
+    setValues([...values, ''])
+    console.log(values)
+  }
+  function handleChange(e: any, i: number) {
+    values[i] = e.target.value
+    console.log(values)
+    setValues(values)
+  }
+
+  return (
+    <div id="gencontainer">
+      <h1 id="gennytitle"> Dummy Data Generator </h1>
+      {/* {fields} */}
+      {values.map((el, i) => {
+        return (
+          <div key={i}>
+            <input value={el} placeholder="Field Name" onChange={(e) => handleChange(e, i)}></input>
+            <select id="dataType" placeholder="Data Type"><option>Data Type</option></select>
+            <div id="percentagediv">
+              <input id="empty" placeholder="% empty"></input><h3>%</h3>
+            </div>
+          </div>
+        )
+      })}
+      <button onClick={addInput} id="addColumn" > Add Another Column</button>
+      <Exports />
+    </div>
+  )
+
 }
 
 export default Generator;
--- src/components/generatorChildren/fields.tsx
+++ src/components/generatorChildren/fields.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
+interface props {
+  number: number;
+  value: string;
+  dataType: string;
+  empty: number;
+  callback : any
+}
 
-const Fields = () => {
-    return (
-    <div>
-      <input placeholder="Field Name"></input>
-      <select placeholder="Data Type"><option>Data Type</option></select>
-     <input placeholder="% empty"></input>
-     <button>Add Another Column</button>
-   </div>
-    )
+const Fields: React.FC<props> = ({ number, value, dataType, empty, callback}) => {
+
+  return (
+    <div className={`geninput`} >
+      <input onChange={callback} className={`${number}`} name={`input-${number}`} value={value} id="input" placeholder="Field Name"></input>
+      <select id="dataType" placeholder="Data Type"><option>Data Type</option></select>
+      <input id="empty" placeholder="% empty"></input><h3>%</h3>
+    </div >
+  )
 }
 
 export default Fields;

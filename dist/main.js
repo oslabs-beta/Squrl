63d79693adacf6a5abab3fbb48405bd7a5cb235f
Andy <a4heng@ucsd.edu>
2020-11-21 11:39:06 -0800

    tscomplier js files

--- dist/main.js
+++ dist/main.js
@@ -2,6 +2,12 @@
 /******/ 	// The module cache
 /******/ 	var installedModules = {};
 /******/
+/******/ 	// object to store loaded chunks
+/******/ 	// "0" means "already loaded"
+/******/ 	var installedChunks = {
+/******/ 		"main": 0
+/******/ 	};
+/******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
 /******/
@@ -26,6 +32,47 @@
 /******/ 		return module.exports;
 /******/ 	}
 /******/
+/******/ 	// This file contains only the entry chunk.
+/******/ 	// The chunk loading function for additional chunks
+/******/ 	__webpack_require__.e = function requireEnsure(chunkId) {
+/******/ 		var promises = [];
+/******/
+/******/
+/******/ 		// ReadFile + VM.run chunk loading for javascript
+/******/
+/******/ 		var installedChunkData = installedChunks[chunkId];
+/******/ 		if(installedChunkData !== 0) { // 0 means "already installed".
+/******/ 			// array of [resolve, reject, promise] means "currently loading"
+/******/ 			if(installedChunkData) {
+/******/ 				promises.push(installedChunkData[2]);
+/******/ 			} else {
+/******/ 				// load the chunk and return promise to it
+/******/ 				var promise = new Promise(function(resolve, reject) {
+/******/ 					installedChunkData = installedChunks[chunkId] = [resolve, reject];
+/******/ 					var filename = require('path').join(__dirname, "/" + ({}[chunkId]||chunkId) + ".js");
+/******/ 					require('fs').readFile(filename, 'utf-8',  function(err, content) {
+/******/ 						if(err) return reject(err);
+/******/ 						var chunk = {};
+/******/ 						require('vm').runInThisContext('(function(exports, require, __dirname, __filename) {' + content + '\n})', filename)(chunk, require, require('path').dirname(filename), filename);
+/******/ 						var moreModules = chunk.modules, chunkIds = chunk.ids;
+/******/ 						for(var moduleId in moreModules) {
+/******/ 							modules[moduleId] = moreModules[moduleId];
+/******/ 						}
+/******/ 						var callbacks = [];
+/******/ 						for(var i = 0; i < chunkIds.length; i++) {
+/******/ 							if(installedChunks[chunkIds[i]])
+/******/ 								callbacks = callbacks.concat(installedChunks[chunkIds[i]][0]);
+/******/ 							installedChunks[chunkIds[i]] = 0;
+/******/ 						}
+/******/ 						for(i = 0; i < callbacks.length; i++)
+/******/ 							callbacks[i]();
+/******/ 					});
+/******/ 				});
+/******/ 				promises.push(installedChunkData[2] = promise);
+/******/ 			}
+/******/ 		}
+/******/ 		return Promise.all(promises);
+/******/ 	};
 /******/
 /******/ 	// expose the modules object (__webpack_modules__)
 /******/ 	__webpack_require__.m = modules;
@@ -79,6 +126,13 @@
 /******/ 	// __webpack_public_path__
 /******/ 	__webpack_require__.p = "";
 /******/
+/******/ 	// uncaught error handler for webpack runtime
+/******/ 	__webpack_require__.oe = function(err) {
+/******/ 		process.nextTick(function() {
+/******/ 			throw err; // catch this error by using import().catch()
+/******/ 		});
+/******/ 	};
+/******/
 /******/
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(__webpack_require__.s = "./electron/main.ts");
@@ -104,6 +158,7 @@ __webpack_require__.r(__webpack_exports__);
 
 
 
+__webpack_require__.e(/*! import() */ 0).then(__webpack_require__.t.bind(null, /*! react-devtools-electron */ "./node_modules/react-devtools-electron/dist/index.js", 7));
 var mainWindow;
 
 function createWindow() {
---
+++ dist/webpack.electron.config.js
@@ -0,0 +1,26 @@
+"use strict";
+const path = require('path');
+module.exports = {
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    devtool: 'source-map',
+    entry: './electron/main.ts',
+    target: 'electron-main',
+    module: {
+        rules: [
+            {
+                test: /\.(js|ts|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].js',
+    },
+};
+//# sourceMappingURL=webpack.electron.config.js.map

9c9fa1a60d2c47ca94cf907c9324d09e16cfeff7
Andy <a4heng@ucsd.edu>
2020-11-21 21:30:11 -0800

    resolved merge conflict

--- electron/main.ts
+++ electron/main.ts
@@ -1,14 +1,8 @@
 //creates shell of desktop application in electron
 
 import { app, BrowserWindow } from "electron";
-import installExtension, {REACT_DEVELOPER_TOOLS} from "electron-devtools-installer"
 import * as path from "path";
 import * as url from "url";
-<<<<<<< HEAD
-import('react-devtools-electron')
-=======
-import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer'
->>>>>>> a0da6c6aa1de646d82c1b857dfe6b542ebc9a930
 
 let mainWindow: Electron.BrowserWindow | null;
 
@@ -21,14 +15,10 @@ function createWindow() {
       nodeIntegration: true,
     },
   });
-<<<<<<< HEAD
   //macOS dock settings for logo
   if (process.platform === 'darwin') {
     app.dock.setIcon("./assets/templogo.png");
   }
-=======
-
->>>>>>> bbceadd0335e240f403b38d4938f4b4ecaeb8413
   if (process.env.NODE_ENV === "development") {
     mainWindow.loadURL(`http://localhost:3000`);
   } else {

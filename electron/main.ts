8cf798a9adba25af5d95511fa067ef169c0cfca1
Andy <a4heng@ucsd.edu>
2020-12-09 21:19:05 -0800

    trying to resolve electron packaging

--- electron/main.ts
+++ electron/main.ts
@@ -4,9 +4,10 @@ import * as path from "path";
 import * as url from "url";
 import fs from 'fs'
 import http from 'http'
+import axios from "axios"
 import express, {Request, Response} from "express";
-import * as fakerRouter from "../backend/routes/fakerRoute"
-import * as modelRouter from "../backend/routes/api"
+import * as fakerRouter from "./backend/routes/fakerRoute"
+import * as modelRouter from "./backend/routes/api"
 import cors from 'cors';
 // const { remote } = window.require('electron')
 // console.log('My path:', remote.app.getAppPath())
@@ -71,12 +72,28 @@ ipcMain.on("download", (event, arg) => {
     if (filePath_obj.canceled)
       console.log("canceled")
     else {
+      // const dest: any = fs.createWriteStream(filePath_obj.filePath + '.sql');
       const dest: any = fs.createWriteStream(filePath_obj.filePath + '.sql');
-      const request = http.get("http://localhost:30000/faker/create", arg, function (response) {
+      // const request = http.get(`http://localhost:30000/faker/create/`,arg, function (response) {
+      //   console.log(arg)
+      //   response.pipe(dest);
+      // });
+      const request = http.get(`http://localhost:30000/faker/create/` , arg, function (response) {
         response.pipe(dest);
       });
+      // axios.post(`http://localhost:30000/faker/create/`, {path: app.getAppPath()}).then((res)=>{
+      //   res.pipe(dest)
+      // })
+      // axios({
+      //   method:"POST",
+      //   url: `http://localhost:30000/faker/create/`,
+      //   responseType: "stream",
+      // }).then(function(response){
+      //   response.data.pipe(dest)
+      // })
     }
-  }).catch(err => {
+  })
+  .catch(err => {
     console.log(err)
   })
 });
